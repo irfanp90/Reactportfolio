6f8f35186c658c18f433f52757b87ab15357178a
irfanp90 <irfanp90@gmail.com>
2019-05-17 17:53:39 -0500

    update

---
+++ models/form.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const formSchema = new Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  
+
+  Date: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+const Form = mongoose.model("Form", formSchema);
+
+module.exports = Form;
