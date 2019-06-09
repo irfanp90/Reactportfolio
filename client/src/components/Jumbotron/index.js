27903c4de2b1b64481979fd6b7048a3680bbce84
irfanp90 <irfanp90@gmail.com>
2019-06-08 23:14:16 -0500

    update jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,31 +1,30 @@
 import React from "react";
-import brick from "../../images/desk.jpg"
+import brick from "../../images/Stock.jpg"
 const headerStyle = {
     
   backgroundImage: `url(${brick})`,
     
     fontWeight:"bold",
     backgroundSize:"cover",
-    textAlign:"center",
+    textAlign:"left",
     opacity: "0.8",
     fontFamily: "Anton sans-serif",
     marginBottom:"0px",
     color:"black",
-    textShadow: " 0 0 0.2em  #6497b1"
-    
+    maxWidth:"100%",
+    height:"auto",
+    textShadow: " 0 0 3px #6497b1"
+
   }
 
   function Jumbotron(){
     return (
       <div className="background">
       <div className=" jumbotron jumbotron-fluid" style={headerStyle}>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-       <p style={{  fontSize: "75px"}}>IRFAN PATEL</p>
+        <div className="container">
+        <p style={{  fontSize: "75px"}}>IRFAN PATEL</p>
         <p style={{  fontSize: "40px"}}>FULL STACK  DEVELOPER</p>
-       
+        </div>
       </div>
      </div>
     );
